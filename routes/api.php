64dd8ee817ef19d0ebdb7239c299957aeaf5a576
Kelvin Severino <kseveri@outlook.com>
2023-08-02 21:02:45 -0300

    Autenticação da API com Sanctum

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,16 @@
 use App\Http\Controllers\Api\{
     UserController,
 };
-use App\Http\Controllers\Api\Auth\RegisterController;
+use App\Http\Controllers\Api\Auth\{
+    RegisterController,
+    AuthController
+};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Auth and Register Routes
 Route::post('/register', [RegisterController::class, 'store']);
+Route::post('/auth', [AuthController::class, 'auth']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('/users', UserController::class);
